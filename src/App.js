add3c1b9bd93db6050417cce1657f5584c5918a6
ivan886 <ivanfernando.fonseca@gmail.com>
2019-12-31 09:14:31 -0500

    integracion

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          integracion continua
         </a>
       </header>
     </div>
